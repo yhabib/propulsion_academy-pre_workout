824f00de3ab84aa6e063455b5243d4e357c5b506
Yusef Habib <habibfernandez@gmail.com>
2017-01-04 01:32:37 +0100

    Factorial exercise and test

---
+++ Java/Pre-Work/src/org/propulsion_academy/pre_work/Factorial.java
@@ -0,0 +1,32 @@
+package org.propulsion_academy.pre_work;
+
+import java.util.stream.IntStream;
+
+public class Factorial {
+	static int factorialStream(int n) {
+		return IntStream.rangeClosed(1, n).reduce(1, (a, b) -> a * b);
+	}
+
+	static int factorialRecursive(int n) {
+		if (n == 0 || n == 1)
+			return 1;
+		return n * factorialRecursive(n - 1);
+	}
+
+	static int factorialIterative(int n) {
+		int factorial = 1;
+		while (n > 0) {
+			factorial *= n;
+			n--;
+		}
+		return factorial;
+	}
+
+	public static void main(String... args) {
+		IntStream.rangeClosed(0, 5).forEach(n -> print(n));
+	}
+
+	private static void print(int n) {
+		System.out.println(n + "! = " + factorialStream(n));
+	}
+}
---
+++ Java/Pre-Work/test/org/propulsion_academy/pre_work/FactorialTest.java
@@ -0,0 +1,21 @@
+package org.propulsion_academy.pre_work;
+
+import junit.framework.TestCase;
+
+public class FactorialTest extends TestCase {
+	public void testFactorialRecursive() {
+		int test = 5;
+		assertEquals(120, Factorial.factorialRecursive(test));
+	}
+
+	public void testFactorialIterative() {
+		int test = 5;
+		assertEquals(120, Factorial.factorialIterative(test));
+	}
+
+	public void testFactorialStream() {
+		int test = 5;
+		assertEquals(120, Factorial.factorialStream(test));
+	}
+
+}
